f3a37aa4851b740030791476bf8340742359425f
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-02-09 22:02:16 -0500

    feat: fix base in vite config and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:DreckSallow/DreckSallow.github.io.git main
+git push -f git@github.com:DreckSallow/DreckSallow.github.io.git main
 
 
 cd -
